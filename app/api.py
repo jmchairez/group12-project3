3ca8a0a511b2a941bd7fdd9d2c19ffc11a90d88a
gcanales85 <gina.canales@gmail.com>
2021-01-23 10:15:29 -0600

    update to flask

---
+++ app/api.py
@@ -0,0 +1,12 @@
+from flask import flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods =["GET","POST"])
+def upload_predict():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port =12000, debug = True)
+    
--- app/app.py
+++ app/app.py
@@ -1,69 +1,57 @@
 import os
+from flask import Flask, flash, render_template, redirect, request, url_for, send_file
+from flask import send_from_directory
 import numpy as np
-import tensorflow as tf
-from flask import Flask, request, redirect,url_for, send_from_directory, jsonify, render_template
-from tensorflow import keras
-from keras.preprocessing import imageDataGenerator, load_img, img_to_array
-from keras import backend as k
 from tensorflow.keras.models import load_model
+from tensorflow import keras
+import tensorflow as tf
+from keras.preprocessing import imageDataGenerator, load_img, img_to_array
 
-ALLOWED_EXTENSIONS = set(['jpg','jpeg','png','tiff'])
-IMAGE_SIZE = (180,180)
-UPLOAD_IMAGE = 'uploads'
-
-#h5 = load_model('model')
-##vgg16 = load_model('model/vgg16.h5')##
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
-
-def predict(file):
-    img = load_img(file, target_size = IMAGE_SIZE)
-    img = img_to_array(img)/225.0
-    img = np.expand_dims(img, axis=0)
-    probs = h5.predict(img)[0]
-    output = {'damage': probs[0], 'no damage': probs[1]}
-    return output
-
+#Define a flask app
 app = Flask(__name__)
-app.config['UPLOAD_IMAGE'] = 'uploads'
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['jpg','jpeg','png','tiff', 'bmp','gif'])
+image_size = (128,128)
+
+#model saved 
+harvey_model3 = load_model(modelgroup2/model/harvey_model3.h5)
+
+def allowed_image(imagename):
+    return '.' in imagename and \
+           imagename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #flask routes#
-@app.route("/")
-def index_template():
-    return render_template('index.html', label = '')
+@app.route('/', methods=['GET', 'POST'])
+def upload_predict():
+    return render_template("index.html")
 
-@app.route('/', methods = ['GET', 'POST'])
-def upload_file():
     if request.method == 'POST':
-        file = request.files["file"]
-        filename = file.filename
-        filepath = os.path.join(app.config['UPLOAD_IMAGE'],filename)
-        file.save(filepath)
-        output = predict(file_path)
-    return render_template(index.html, label = output, imagesource = file_path)
+        # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
 
 @app.route('/uploads/<filename>')
-def upload_file(filename):
-    return send_from_directory(app.config ['UPLOAD_IMAGE'], filename)
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
-# @app.route("/photo", methods = ["GET", "POST"])
-# def predict():
-#     model = load_model("data/final_model.h5")
-
-#     data = {"Success": False}
-
-#     if request.method == "POST":
-#         if request.files.get("file"):
-#             file = request.files["file"]
-#             filename = file.filename
-#             filepath = f"{app.config['UPLOAD_IMAGE']}/{filename}"
-#             file.save(filepath)
-
-#             image_size = (180,180)
-#             im = image.load_img(filepath,target_size=image_size)
-#             image_array = prepare_image(im)
+@app.route('/predict/<filename>', methods =['GET'])
+def predict(filename):
+    img = load_img(file, target_size = image_size)
+    img = img_to_array(img)/225.0
+    img = np.expand_dims(img, axis=0)
+    probs = harvey_model3.predict(img)[0]
+    output = {'damage': probs[0], 'no damage': probs[1]}
+    return output
+    
 if __name__ == "__main__":
     load_model()
     app.run(debug=True)
